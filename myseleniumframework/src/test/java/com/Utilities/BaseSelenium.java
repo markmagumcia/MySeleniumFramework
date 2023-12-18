f5a35f5a751605f5748f352bb40ba3c56e3c6fe5
Mark Magumcia <mark.magumcia@anglefinance.com.au>
2023-12-18 11:16:00 +0800

    Added Test data

---
+++ myseleniumframework/src/test/java/com/Utilities/BaseSelenium.java
@@ -0,0 +1,64 @@
+package com.Utilities;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class BaseSelenium {
+    public static Logger log = LogManager.getLogger();
+	private static String URL = "https://jupiter.cloud.planittesting.com/";
+	static WebDriver driver;
+	static WebDriverWait wait;
+
+    public static void launchBrowser() {
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		log.info("Launching: {}", URL);
+        driver.manage().window().fullscreen();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(URL);
+
+	}
+    	public static void closeBrowser() {
+		log.info("Closing: {} ", URL);
+		driver.quit();
+	}
+
+	public void verifyElementIsDisplayed(String xpath) {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+		assertTrue(driver.findElement(By.xpath(xpath)).isDisplayed());
+	}
+
+	public void verifyElementIsNotDisplayed(String xpath) {
+		assertTrue(driver.findElements(By.xpath(xpath)).size() == 0);
+	}
+
+	public void enterText(String xpath, String text) {
+		WebElement element = driver.findElement(By.xpath(xpath));
+		element.clear();
+		element.sendKeys(text);
+	}
+
+	public String getText(String xpath) {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+		return driver.findElement(By.xpath(xpath)).getText();
+	}
+
+
+}
