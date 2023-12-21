ca28ea4cd363666162e99f1dfddc2dcfa7d1bda7
Mark Magumcia <mark.magumcia@anglefinance.com.au>
2023-12-21 12:19:27 +0800

    used for each for webelements

--- myseleniumframework/src/test/java/com/UIMaps/POM/sauceDemoPOM.java
+++ myseleniumframework/src/test/java/com/UIMaps/POM/sauceDemoPOM.java
@@ -2,6 +2,8 @@ package com.UIMaps.POM;
 
 import com.setup.BaseSelenium;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -32,32 +34,38 @@ public class sauceDemoPOM {
    }
 
     public void makeInventoryJSON(){
-        String xpathString,itemName,itemDesc,itemPrice;
+        String itemName,itemDesc,itemPrice;
         JsonHelper json = new JsonHelper();
         ObjectNode resultJSON = json.createParentJSON();
-        WebElement element;
+        WebElement element,elemento;
         int dashboardItemsCount;
         dashboardItemsCount= selenium.findElements(sauceDemoXpaths.INVENTORY_ITEMS).size();
 
-        
-        for (int i = 1; i<=dashboardItemsCount; i++){
-            ObjectNode childNode=json.createChildJSON();
-            String xpath = "//div[@class='inventory_item']["+i+"]";
-            element = selenium.findElement(By.xpath(xpath));
-            itemName = element.findElement(By.xpath("//div[@class='inventory_item_name ']")).getText();
-            itemDesc = element.findElement(By.xpath("//div[@class='inventory_item_desc']")).getText();
-            itemPrice = element.findElement(By.xpath("//div[@class='inventory_item_price']")).getText();
-            json.addFieldsToChild(childNode,"itemName", itemName);
-            json.addFieldsToChild(childNode,"itemDesc", itemDesc);
-            json.addFieldsToChild(childNode,"itemPrice", itemPrice);
-            json.addChildToRoot("Test Data#"+i, resultJSON, childNode);
-            try {
-                System.out.println(json.outputRoot(resultJSON));
-            } catch (JsonProcessingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+        List<WebElement> listOfElements = selenium.findElements("//div[@class='inventory_item']");
+        for (WebElement e: listOfElements){
+            System.out.println(e.getText());
         }
+        
+        // for (int i = 1; i<=dashboardItemsCount; i++){
+        //     ObjectNode childNode=json.createChildJSON();
+        //     String xpath = "//div[@class='inventory_item']";
+        //     element = selenium.findElements(xpath).get(i);
+        //     elemento = element.findElement(By.xpath("//a"));
+        //     System.out.println(elemento.getAttribute("id"));
+        //     itemName = element.findElement(By.xpath("//div[@class='inventory_item_name ']")).getText();
+        //     itemDesc = element.findElement(By.xpath("//div[@class='inventory_item_desc']")).getText();
+        //     itemPrice = element.findElement(By.xpath("//div[@class='inventory_item_price']")).getText();
+        //     json.addFieldsToChild(childNode,"itemName", itemName);
+        //     json.addFieldsToChild(childNode,"itemDesc", itemDesc);
+        //     json.addFieldsToChild(childNode,"itemPrice", itemPrice);
+        //     json.addChildToRoot("Test Data#"+i, resultJSON, childNode);
+        //     try {
+        //         System.out.println(json.outputRoot(resultJSON));
+        //     } catch (JsonProcessingException e) {
+        //         // TODO Auto-generated catch block
+        //         e.printStackTrace();
+        //     }
+        //}
         
         
     }
