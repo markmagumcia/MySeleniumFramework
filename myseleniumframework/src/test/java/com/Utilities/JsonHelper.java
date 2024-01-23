ee89a0a38e915b9f80b5307450d499e86984ba20
markddmagumcia@gmail.com <markddmagumcia@gmail.com>
2024-01-23 11:38:01 -0500

    corrected jsonhelper

--- myseleniumframework/src/test/java/com/Utilities/JsonHelper.java
+++ myseleniumframework/src/test/java/com/Utilities/JsonHelper.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-d
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
